4d5557dc3173e53504de2a66dc8d8d30ac9d0061
gb9900 <gautier.black@student.uclouvain.be>
2024-01-10 18:30:42 +0100

    translate comments in english

--- Annotation/ROI.py
+++ Annotation/ROI.py
@@ -5,61 +5,66 @@ import sys
 import os
 
 #image path
-img_path= sys.argv[1]
+img_path = sys.argv[1]
 
-#read image
-img_raw = cv2.imread(img_path)
-height, width, channels = img_raw.shape
+#read image + save dimensions
+img = cv2.imread(img_path)
+height, width, _ = img.shape
 
 #select ROIs GUI
-ROIs = cv2.selectROIs("Select samples",img_raw,True,False)
+ROIs = cv2.selectROIs("Select samples",img,True,False)
 
 #counter to save image with different name
-count_id=0
+count_id = 0
 
 #identification class
 class_id = 0
 
 #folder for annotation
-data_annot = "annot"
-if not os.path.exists(data_annot):
-    os.makedirs(data_annot)
-image_filename_sp, _ = os.path.splitext(os.path.basename(img_path))
-image_filename_txt = image_filename_sp + ".txt"
-output_file_path = os.path.join(data_annot,image_filename_txt)
+data_annotation = "Annotation"
+if not os.path.exists(data_annotation):
+    os.makedirs(data_annotation)
+#set the output annotation file path
+image_filename, _ = os.path.splitext(os.path.basename(img_path))
+image_filename_txt = image_filename + ".txt"
+output_file_path = os.path.join(data_annotation,image_filename_txt)
 
 #folder for individuals images
-path = 'ROI'
-if not os.path.exists(path):
-    os.makedirs(path)           
-if not os.path.exists(os.path.join(path,image_filename_sp)):
-    #sous-dossier
-    os.mkdir(os.path.join(path,image_filename_sp))
+data_indiv = 'ROI'
+if not os.path.exists(data_indiv):
+    os.makedirs(data_indiv) 
+#sub-folder             
+if not os.path.exists(os.path.join(data_indiv,image_filename)):
+    os.mkdir(os.path.join(data_indiv,image_filename))
     
-#for every box
+#open annotation file
 with open(output_file_path, 'w') as output_file:
+    #for every ROI
     for rect in ROIs:
-            xi=rect[0]
-            yi=rect[1]
-            w=rect[2]
-            h=rect[3]
-            
-            xf = xi+w
-            yf = yi+h
-
-            #crop roi from original image
-            img_crop=img_raw[yi:yf,xi:xf]
-            
-            x_center = (xi + (xf-xi)/ 2) / width
-            y_center = (yi + (yf-yi)/ 2) / height
-            width_normalized = (xf-xi) / width
-            height_normalized = (yf-yi) / height
-            
-            output_file.write(f"{class_id} {x_center} {y_center} {width_normalized} {height_normalized}\n")
-
         
-	        #save cropped image
-            cv2.imwrite(os.path.join(os.path.join(path,image_filename_sp) , 'ROI'+str(count_id)+'.png'), img_crop)
+        #take coordinates
+        x_i = rect[0]
+        y_i = rect[1]
+        w = rect[2]
+        h = rect[3]
+        
+        x_f = x_i+w
+        y_f = y_i+h
 
-            count_id+=1
+        #crop ROI from original image
+        img_crop = img[y_i:y_f,x_i:x_f]
+        
+        #normalized coordinates
+        x_center = (x_i + (x_f-x_i)/ 2) / width
+        y_center = (y_i + (y_f-y_i)/ 2) / height
+        width_normalized = (x_f-x_i) / width
+        height_normalized = (y_f-y_i) / height
+        
+        #write in the anntation file
+        output_file.write(f"{class_id} {x_center} {y_center} {width_normalized} {height_normalized}\n")
 
+    
+        #save cropped image
+        cv2.imwrite(os.path.join(os.path.join(data_indiv,image_filename) , 'ROI'+str(count_id)+'.png'), img_crop)
+
+        count_id+=1
--- Reconstitution_boite/crop.py
+++ Reconstitution_boite/crop.py
@@ -5,61 +5,70 @@ import sys
 import cv2
 import numpy as np
 
-filenameimg = sys.argv[1]
-cropx = int(sys.argv[2])
-cropy = int(sys.argv[3])
+#folder path + crop to apply
+folder_path = sys.argv[1]
+crop_x = int(sys.argv[2])
+crop_y = int(sys.argv[3])
 
-#coordonnées de la zone de comptage
-pyi = 0+2*cropy
-pyf = 1080-0*cropy
-pxi = 0+1*cropx
-pxf = 1920-1*cropx
+#coordinates of the cropped zone
+y_i = 0+2*crop_y
+y_f = 1080-0*crop_y
+x_i = 0+1*crop_x
+x_f = 1920-1*crop_x
 
-#nb d'images en x et en y
-if len(os.listdir(filenameimg))>36:
-    if len(os.listdir(filenameimg))>42:
-        ny = 12
+#determine the height of the box (number of images in y) according to the total number of images in the folder
+if len(os.listdir(folder_path))>36:
+    if len(os.listdir(folder_path))>42:
+        height_box = 12
     else:
-        ny = 7
+        height_box = 7
 else:
-    ny = 6
-    
-nx = 6
+    height_box = 6
 
-dy = pyf-pyi
-dx = pxf-pxi
+#width of the box (number of images in x) is always 6   
+width_box = 6
 
-height = (ny-2)*dy+(1080-pyi)+pyf
-width = (nx-2)*dx+(1920-pxi)+pxf
+delta_y = y_f-y_i
+delta_x = x_f-x_i
 
-boite = Image.new('RGB', (width, height))
-#dossier contenant les différentes parties de la boite
-data_directory = filenameimg
+#determine the size of the reconstructed image
+height = (height_box-2)*delta_y+(1080-y_i)+y_f
+width = (width_box-2)*delta_x+(1920-x_i)+x_f
+
+#creating a blank image
+box = Image.new('RGB', (width, height))
+
 count = 0
-for image_filename in sorted(os.listdir(data_directory),reverse=True):
-    image_path = os.path.join(data_directory, image_filename)
-    cuti = Image.open(image_path)
+
+#cuts all folder images to defined dimensions
+for image_filename in sorted(os.listdir(folder_path),reverse=True):
+    image_path = os.path.join(folder_path, image_filename)
+    to_cut = Image.open(image_path)
     
-    xx = (count//ny)
-    yy = (count%ny)
+    #x and y coordinates of the part
+    x_part = (count//height_box)
+    y_part = (count%height_box)
     
-    pxii = pxi
-    pyii = pyi
-    pxff = pxf
-    pyff = pyf
+    #create local variables to modify the zone if needed
+    local_y_i = y_i
+    local_y_f = y_f
+    local_x_i = x_i
+    local_x_f = x_f
     
+    #if it is an edge, extend the zone up to it
+    if(x_part == 0): 
+        local_x_i = 0
+    if(x_part == width_box-1):
+        local_x_f = 1920  
+    if(y_part == 0):
+        local_y_i = 0
+    if(y_part == height_box-1):
+        local_y_f = 1080
     
-    if(xx == 0): 
-        pxii = 0
-    if(xx == nx-1):
-        pxff = 1920  
-    if(yy == 0):
-        pyii = 0
-    if(yy == ny-1):
-        pyff = 1080
-        
-    cutf = cuti.crop((pxii, pyii, pxff, pyff))
-    boite.paste(cutf,(pxii+xx*dx,pyii+yy*dy))
+    #cuts and places in the right place on the reconstructed image
+    cut = to_cut.crop((local_x_i, local_y_i, local_x_f, local_y_f))
+    box.paste(cut,(local_x_i+x_part*delta_x,local_y_i+y_part*delta_y))
     count = count+1
 
-boite.save("crop.jpg")
+#save the result
+box.save("crop.jpg")
