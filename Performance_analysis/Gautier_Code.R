7a02bf8a751e7d5a3b25c32b2455f5bec8ab3afd
gb9900 <gautier.black@student.uclouvain.be>
2024-01-15 10:40:02 +0100

    rename folders in english

--- Performance_analysis/Gautier_Code.R
+++ Performance_analysis/Gautier_Code.R
@@ -1,37 +1,33 @@
-setwd(dir = "Bureau/TFE/stat/1/")
+#load data
+setwd(dir = "")
 
 Gautier_Data<-read.csv2("Gautier_Data.csv")
 str(Gautier_Data)
 Gautier_Data$method<-as.factor(Gautier_Data$method)
 
-library(lme4)
+#libraries
 library(multcompView)
 library(readxl)
 library(ggplot2)
-library(reshape2)  # for melt() function
+library(reshape2)
 library(corrplot)
 library(RColorBrewer)
 library(dplyr)
-library(tidyverse)
 library(broom)
 library(ggeffects)
 library(stargazer)
 library(ggfortify)
 library(lsmeans)
 library(emmeans)
-library(car)
 library(ggplot2)
 library(Hmisc)
 library(vegan)
 library(ggsci)
-library(rstatix)
-library(ggpubr)
 library(sandwich)
 library(msm)
 
 ####Exploratory data######
-#Check the variability of our customized precision formulae
-library(hrbrthemes)
+#Check the variability of the customized precision formula
 ggplot(data=Gautier_Data, aes(y = precision, x = method, fill = method)) +
   geom_boxplot(fill = c("#DA5724","cyan")) + geom_jitter(color="black", alpha=0.9) + theme_bw()
 GBXMethod<-Gautier_Data[1:31,]
@@ -39,10 +35,11 @@ TervurenMethod<-Gautier_Data[32:59,]
 GBXMethod <- GBXMethod$precision
 TervurenMethod <- TervurenMethod$precision
 
-##Test_Stats
+#Test_Stats
 t.test(GBXMethod,TervurenMethod)
-### ==> Significant test so there is a precision value that is different with the TERVUREN method
+# ==> Significant test so there is a precision value that is different with the TERVUREN method
 
+#same on absolute value
 ggplot(data=Gautier_Data, aes(y = absolute_precision, x = method, fill = method)) +
   geom_boxplot(fill = c("#DA5724","cyan")) + geom_jitter(color="black", alpha=0.9) + theme_bw()
 GBXMethod<-Gautier_Data[1:31,]
@@ -50,9 +47,9 @@ TervurenMethod<-Gautier_Data[32:59,]
 GBXMethod <- GBXMethod$absolute_precision
 TervurenMethod <- TervurenMethod$absolute_precision
 
-##Test_Stats
+#Test_Stats
 t.test(GBXMethod,TervurenMethod)
-### ==> Significant test so there is a precision value that is different with the TERVUREN method
+# ==> Significant test so there is a precision value that is different with the TERVUREN method
 
 ggplot(data=Gautier_Data, aes(y = heightmean, x = method, fill = method)) +
   geom_boxplot(fill = c("orange","#DA5724")) + geom_jitter(color="black", alpha=0.9)
@@ -68,7 +65,7 @@ ggplot(data=Gautier_Data, aes(y = heightmean, x = method, fill = method)) +
 #widthmean = Largeur moyenne des contours sélectionnés
 #heightmean = Hauteur moyennes des contours sélectionnés
 
-####LM
+#LM
 pairs2(Gautier_Data[,c(17 : 27)])
 
 lm.preci<- lm(precision ~ widthmean + size_conservation + counting_zone + density + overlap + variability +colored_variability, data = Gautier_Data)
@@ -77,7 +74,7 @@ lm.preci<- lm(absolute_precision ~ real_density, data = Gautier_Data)
 summary(lm.preci)
 
 
-###Modèle mixte en fonction du site
+#Modèle mixte en fonction du site
 
 ggplot(data=Gautier_Data, aes(y = precision, x = size_conservation)) +
   geom_point(shape = 1) +
@@ -120,3 +117,4 @@ ggplot(data=Gautier_Data, aes(y = absolute_precision, x = real_number, fill = me
   geom_point(shape = 1) +
   geom_smooth(method = "lm",se = T, color = "black")+
   theme_bw()
+
